f276ea53aa59b944f086e86bd1ab55f4503f1fdc
Emily Nguyen <emn001@ucsd.edu>
2018-02-17 00:24:05 -0800

    Edit about

--- routes/home.js
+++ routes/home.js
@@ -9,6 +9,6 @@ exports.view = function(req, res){
   res.render('home', {
  		data,
  		"title" : "Emily Nguyen - Developer & Designer",
- 		"description" : "Hiya, my name is Emily! I’m a third year student at UC San Diego, pursuing a degree in Cognitive Science: Human-Computer Interaction and a minor in Computer Science."
+ 		"description" : "Hiya, my name is Emily! I’m a third year student at UC San Diego, pursuing a degree in Cognitive Science: Human-Computer Interaction and a minor in Computer Science. I love crafting visual experiences and seeing them through to implementation."
   });
 };
