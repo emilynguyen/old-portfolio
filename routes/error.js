6c7ba38d8c5a7b8b3a9bff146a74941cfad89522
Emily Nguyen <emn001@ucsd.edu>
2018-03-30 23:19:05 -0700

    Update tagline and page titles

--- routes/error.js
+++ routes/error.js
@@ -9,7 +9,7 @@ exports.view = function(req, res){
 	console.log("error page");
   res.render('404', {
   	data,
-  	"title" : "Page Not Found",
+  	"title" : "Page Not Found | Emily Nguyen",
   	"description" : "Oops! The page you're looking for does not exist."
   });
 };
