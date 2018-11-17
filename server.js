4abc80daf41662d6f09996520db34db37b81e207
Emily Nguyen <emn001@ucsd.edu>
2018-11-16 20:04:51 -0800

    Fix

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.get("/about", about.view);
 app.get("/:title", project.view);
 app.get("*", error.view);
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Express server listening on port " + app.get("port"));
 });
